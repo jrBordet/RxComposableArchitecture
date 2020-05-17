6c2705192ab278c1660a77eff57e8e61a79df52b
Jean Raphaël Bordet <jr.bordet@synesthesia.it>
2020-05-17 17:06:38 +0200

    Initial commit

---
+++ RxComposableArchitecture/RxComposableArchitecture/RxComposableArchitecture.h
@@ -0,0 +1,19 @@
+//
+//  RxComposableArchitecture.h
+//  RxComposableArchitecture
+//
+//  Created by Jean Raphael Bordet on 17/05/2020.
+//  Copyright © 2020 Jean Raphael Bordet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxComposableArchitecture.
+FOUNDATION_EXPORT double RxComposableArchitectureVersionNumber;
+
+//! Project version string for RxComposableArchitecture.
+FOUNDATION_EXPORT const unsigned char RxComposableArchitectureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxComposableArchitecture/PublicHeader.h>
+
+
