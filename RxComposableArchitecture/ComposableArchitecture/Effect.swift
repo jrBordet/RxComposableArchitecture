b1ebce24c0220b416edec91fb7a1493eadee978a
Jean Raphaël Bordet <jr.bordet@synesthesia.it>
2022-01-05 17:12:21 +0100

    wip

--- RxComposableArchitecture/ComposableArchitecture/Effect.swift
+++ RxComposableArchitecture/ComposableArchitecture/Effect.swift
@@ -9,6 +9,68 @@
 import Foundation
 import RxSwift
 
+public struct SideEffect<Output>: ObservableType {
+	public typealias Element = Output
+	
+	public let upstream: Observable<Output>
+	
+	public init(_ observable: Observable<Output>) {
+	  self.upstream = observable
+	}
+
+	public func subscribe<Observer>(
+		_ observer: Observer
+	) -> Disposable where Observer : ObserverType, Element == Observer.Element {
+		upstream.subscribe(observer)
+	}
+	
+}
+
+extension SideEffect {
+	static func just(value: Output) -> SideEffect {
+		.init(Observable.just(value))
+	}
+	
+	public static var none: SideEffect {
+		Observable.empty().eraseToEffect()
+	}
+}
+
+extension ObservableType {
+  /// Turns any publisher into an `Effect`.
+  ///
+  /// This can be useful for when you perform a chain of publisher transformations in a reducer, and
+  /// you need to convert that publisher to an effect so that you can return it from the reducer:
+  ///
+  ///     case .buttonTapped:
+  ///       return fetchUser(id: 1)
+  ///         .filter(\.isAdmin)
+  ///         .eraseToEffect()
+  ///
+  /// - Returns: An effect that wraps `self`.
+  public func eraseToEffect() -> SideEffect<Element> {
+	  SideEffect(asObservable())
+  }
+
+  /// Turns any publisher into an `Effect` that cannot fail by wrapping its output and failure in a
+  /// result.
+  ///
+  /// This can be useful when you are working with a failing API but want to deliver its data to an
+  /// action that handles both success and failure.
+  ///
+  ///     case .buttonTapped:
+  ///       return fetchUser(id: 1)
+  ///         .catchToEffect()
+  ///         .map(ProfileAction.userResponse)
+  ///
+  /// - Returns: An effect that wraps `self`.
+  public func catchToEffect() -> SideEffect<Result<Element, Error>> {
+	self.map(Result<Element, Error>.success)
+	  .catchError { Observable<Result<Element, Error>>.just(Result.failure($0)) }
+	  .eraseToEffect()
+  }
+}
+
 public enum EffectError: Error {
     case generic
     case custom(String)
